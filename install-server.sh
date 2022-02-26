341ec58c35d473735d1d71cdf731833bbda523e6
Mohamed Elzarei <mohamed.elzarei@tum.de>
2022-02-26 22:58:50 +0000

    fix: server installer

--- install-server.sh
+++ install-server.sh
@@ -7,7 +7,6 @@ if [ -d "$DIR" ]; then
   echo "Skipping Dependencies Installation ${DIR} found... (Delete Directory if want to force install deps)"
 else
   # Download antlr4
-  # shellcheck disable=SC2086
   mkdir "$DIR" && cd "$DIR" && wget https://www.antlr.org/download/antlr-4.9.3-complete.jar
 
   # download pkg-config and uuid
@@ -16,19 +15,16 @@ else
   cd ~/deps/debs && apt download pkg-config && dpkg -x pkg-config*.deb ~/deps/tools
   cd ~/deps/debs && apt download uuid-dev && dpkg -x uuid-dev*.deb ~/deps/tools
 
-  echo "export PATH=$PATH:$HOME/deps/tools/usr/bin" >>~/.bashrc
+  echo "export PATH$HOME/deps/tools/usr/bin:\$PATH" >>~/.bashrc
   echo "export PKG_CONFIG_PATH=$HOME/deps/tools/usr/lib/x86_64-linux-gnu/pkgconfig" >>~/.bashrc
-  source "$HOME"/.bashrc
 fi
 
-
-if [ "$1" == "--standalone" ]
-then
-  cd "$HOME"/jit-regex && cmake -S standalone -B build/standalone -G Ninja -DANTLR_EXECUTABLE="$HOME/antlr4/antlr-4.9.3-complete.jar"
-  cd "$HOME"/jit-regex && cmake --build build/standalone
-  echo "Usage: $HOME/jit-regex/build/standalone/ZRegex -r Regex -b \"llvm\""
-else
+if [ "$1" == "--benchmark" ]; then
   cd "$HOME"/jit-regex && cmake -S benchmark -B build/benchmark -G Ninja -DANTLR_EXECUTABLE="$HOME/antlr4/antlr-4.9.3-complete.jar"
   cd "$HOME"/jit-regex && cmake --build build/benchmark
   echo "Usage: $HOME/jit-regex/build/benchmark/ZRegex_BM"
+else
+  cd "$HOME"/jit-regex && cmake -S standalone -B build/standalone -G Ninja -DANTLR_EXECUTABLE="$HOME/antlr4/antlr-4.9.3-complete.jar"
+  cd "$HOME"/jit-regex && cmake --build build/standalone
+  echo "Usage: $HOME/jit-regex/build/standalone/ZRegex -r Regex -b \"llvm\""
 fi
