3e29dbbcc71c281c9ac9fef69c09fd4e2328738d
Mohamed <mohamed.elzarei@motius.de>
2022-02-26 23:12:17 +0100

    feat: script to run on lrz server without root

---
+++ install-server.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -x
+# LLVM must have been installed or compiled from source
+DIR="$HOME/antlr4"
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "Skipping Dependencies Installation ${DIR} found... (Delete Directory if want to force install deps)"
+else
+  # Download antlr4
+  # shellcheck disable=SC2086
+  mkdir "$DIR" && cd "$DIR" && wget https://www.antlr.org/download/antlr-4.9.3-complete.jar
+
+  # download pkg-config and uuid
+  mkdir -p ~/deps/debs
+  mkdir -p ~/deps/tools
+  cd ~/deps/debs && apt download pkg-config && dpkg -x pkg-config*.deb ~/deps/tools
+  cd ~/deps/debs && apt download uuid-dev && dpkg -x uuid-dev*.deb ~/deps/tools
+
+  echo "export PATH=$PATH:$HOME/deps/tools/usr/bin" >>~/.bashrc
+  echo "export PKG_CONFIG_PATH=$HOME/deps/tools/usr/lib/x86_64-linux-gnu/pkgconfig" >>~/.bashrc
+  source "$HOME"/.bashrc
+fi
+
+
+if [ "$1" == "--standalone" ]
+then
+  cd "$HOME"/jit-regex && cmake -S standalone -B build/standalone -G Ninja -DANTLR_EXECUTABLE="$HOME/antlr4/antlr-4.9.3-complete.jar"
+  echo "Usage: $HOME/jit-regex/build/standalone/ZRegex -r Regex -b \"llvm\""
+else
+  cd "$HOME"/jit-regex && cmake -S benchmark -B build/benchmark -G Ninja -DANTLR_EXECUTABLE="$HOME/antlr4/antlr-4.9.3-complete.jar"
+  echo "Usage: $HOME/jit-regex/build/benchmark/ZRegex_BM"
+fi
